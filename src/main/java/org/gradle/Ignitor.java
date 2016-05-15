de36c8936aef0f5e4f2f1f8b94d8379627819f3f
santhoshnelluri <santhosh.nelluri@gmail.com>
2016-05-14 20:14:46 -0500

    added more system errors

--- src/main/java/org/gradle/Ignitor.java
+++ src/main/java/org/gradle/Ignitor.java
@@ -3,5 +3,10 @@ package org.gradle;
 public class Ignitor {
 	public static void main(String[] args){
 		System.err.println("Hello World...");
+		System.err.println("Hello World...111");
+		System.err.println("Hello World...222");
+		System.err.println("Hello World...333");
+		System.err.println("Hello World...444");
+		System.err.println("Hello World...555");
 	}
 }
